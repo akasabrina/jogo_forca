917386db0f7fdd59ee274ff4e6901256a2132ed2
Sabrina Gaspar <75753093+akasabrina@users.noreply.github.com>
2023-11-06 18:09:33 -0300

    update forca.py
    adição do tema escolhido para não esquecer

--- projeto_forca/forca.py
+++ projeto_forca/forca.py
@@ -5,14 +5,14 @@ import random
 
 def tema(num):
     match num:
-        case 0:
-            return "animais"
         case 1:
-            return "profissoes"
+            return "animais"
         case 2:
+            return "profissoes"
+        case 3:
             return "paises"
         case _:
-            return "Insira um numero de 0 a 2"
+            return "Insira um numero de 1 a 3"
 
 def dificuldade(num):
     match num:
@@ -23,10 +23,9 @@ def dificuldade(num):
         case 3: 
             return int(5)
 
-def carrega_palavra_secreta(num):
+def carrega_palavra_secreta(tema_palavra):
     palavras = []
-    tema_palavra = tema(num)
-    with open(f"tema/{tema_palavra}.txt", "r", encoding="utf-8") as arquivo:
+    with open(f"projeto_forca/tema/{tema_palavra}.txt", "r", encoding="utf-8") as arquivo:
         for linha in arquivo:
             linha = linha.strip()
             palavras.append(linha)
@@ -69,15 +68,16 @@ def jogar():
     while(flag==True):
         print("\n-------- Bem vindo ao jogo da Forca! --------\n")
 
-        print("Escolha um tema para jogar.\n[0] Animais\n[1] Profissões\n[2] Países")
+        print("Escolha um tema para jogar.\n[1] Animais\n[2] Profissões\n[3] Países")
         num_tema = int(input("Tema: "))
+        tema_palavra = tema(num_tema)
         print("\nA dificuldade está atrelada a quantidade de chances que você tem.")
         print("Escolha a dificuldade que quer jogar.\n[1] Fácil - 9 chances\n[2] Médio - 7 chances\n[3] Difícil - 5 chances")
         num_dificuldade = int(input("Dificuldade: "))
         chances = dificuldade(num_dificuldade)
         os.system("cls")
 
-        palavra_secreta = carrega_palavra_secreta(num_tema)
+        palavra_secreta = carrega_palavra_secreta(tema_palavra)
         palavra_secreta2 = unidecode(palavra_secreta)
 
         letras_acertadas = inicializa_letras(palavra_secreta)
@@ -102,7 +102,7 @@ def jogar():
 
                 letras_usadas.append(chute)
                 letras_faltando = str(letras_acertadas.count('_'))
-                print(f"\nLetras usadas: {letras_usadas}\n")
+                print(f"Tema: {tema_palavra}\nLetras usadas: {letras_usadas}\n")
                 print('Ainda faltam acertar {} letras'.format(letras_faltando))
                 print('Você ainda tem {} tentativas'.format(chances-erros))
                 desenha_forca(num_dificuldade, erros)
@@ -129,7 +129,6 @@ def jogar():
             os.system("cls")
         else:
             flag = False
-
 
 if(__name__ == '__main__'):
     jogar()
